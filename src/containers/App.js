4e265c32256fa70df53464a5840382dac7088132
Ahmed Soliman <soliman@uber.com>
2021-08-15 18:12:06 +0200

    add hooks

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,47 +1,39 @@
-import React, {Component} from 'react';
+import React, { useState, useEffect} from 'react';
 import CardList from '../components/CardList';
 import SearchBox from '../components/SearchBox';
 import Scroll from '../components/Scroll';
 import './App.css'
 
-class App extends Component {
-	constructor() {
-		super();
-		this.state = {
-			'robots': [],
-			'searchfield': ''
-		}
-	}
+function App () {
 
-	componentDidMount() {
+	const [robots, setRobots] = useState([]);
+	const [searchfield, setSearchfield] = useState('');
+
+	useEffect(() => {
+		console.log('Loading users data...');
 		fetch('https://jsonplaceholder.typicode.com/users')
 			.then(response => response.json())
-			.then(robots => this.setState({robots:robots}));
-	}
+			.then(robots => setRobots(robots));
+	},[]);
 
-	onSearchChange = (event) => {
-		console.log(event.target.value);
-		this.setState({searchfield: event.target.value});
-	}
+	const onSearchChange = (event) => setSearchfield(event.target.value)
 
-	render() {
-		const { robots, searchfield } = this.state;
-		const filteredRobots = robots.filter(robot => {
-			return robot.name.toLowerCase().includes(searchfield.toLowerCase())
-		});
-		if (!robots.length) {
-			return <h1>Loading...</h1>
-		} else {
-			return (
-				<div className='tc'>
-					<h1 className='f1'>RoboFriends</h1>
-					<SearchBox searchChange={this.onSearchChange}/>
-					<Scroll>
-						<CardList robots={filteredRobots}/>
-					</Scroll>
-				</div>
-			);
-		}
+	const filteredRobots = robots.filter(robot => {
+		return robot.name.toLowerCase().includes(searchfield.toLowerCase())
+	});
+
+	if (!robots.length) {
+		return <h1>Loading...</h1>
+	} else {
+		return (
+			<div className='tc'>
+				<h1 className='f1'>RoboFriends</h1>
+				<SearchBox searchChange={onSearchChange}/>
+				<Scroll>
+					<CardList robots={filteredRobots}/>
+				</Scroll>
+			</div>
+		);
 	}
 }
 
